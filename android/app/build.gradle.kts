d01549bd13c3d00ae0f4334141891cc988a363dc
Matthew Pullen <matthew.pullen@gmail.com>
2025-08-12 23:01:21 -0500

    Build version iOS and Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.makerere_clean"
+    namespace = "com.makerereuniversity.schoolofmedicine"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.makerere_clean"
+        applicationId = "com.makerereuniversity.schoolofmedicine"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
